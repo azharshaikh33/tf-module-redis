191285ff22fd5adf8f5352495154e8b7b9485ea9
Azhar <azharshaikh33@gmail.com>
2024-05-22 11:02:09 +0400

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -19,33 +19,3 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = "roboshop-${var.ENV}-redis-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
-
-
-
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "roboshop-${var.ENV}-docdb"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-# #   backup_retention_period = 5
-# #   preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
-#   db_subnet_group_name    = aws_docdb_subnet_group.docdb_subnet_group.name
-
-# }
-
-# resource "aws_docdb_subnet_group" "docdb_subnet_group" {
-#   name       = "roboshop-${var.ENV}-docdb"
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
-#   tags = {
-#     Name = "roboshop-${var.ENV}-docdb-subnet-group"
-#   }
-# }
-
-# resource "aws_docdb_cluster_instance" "cluster_instances" {
-#   count              = 1
-#   identifier         = "roboshop-${var.ENV}-docdb-instance"
-#   cluster_identifier = aws_docdb_cluster.docdb.id
-#   instance_class     = "db.t3.medium"
-# }
