d78a230d3f692820904228035a195ac13b54c985
Azhar <azharshaikh33@gmail.com>
2024-05-22 07:21:59 +0400

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -6,6 +6,7 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = "6.2"
   port                 = 6379
+  security_group_ids   = [aws_security_group.allow_redis.id]
 }
 
 resource "aws_elasticache_parameter_group" "default" {
