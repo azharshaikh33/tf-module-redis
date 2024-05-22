00936254965d2c9ee48934c4ce596ad0e8f30aa4
Azhar <azharshaikh33@gmail.com>
2024-05-22 10:14:34 +0400

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -4,14 +4,14 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
   security_group_ids   = [aws_security_group.allow_redis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 }
 
 resource "aws_elasticache_parameter_group" "default" {
-  name   = "roboshop-${var.ENV}-redis"
+  name   = "roboshop-${var.ENV}-redis-pg"
   family = "redis6.x"
 }
 
